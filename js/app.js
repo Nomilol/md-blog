96aa176ca15ee4b4a1b9eb7ac9c8f27dbaa0a1a0
Axel <lezinaxel@gmail.com>
2016-10-25 11:22:37 +0200

    Affichage de la page Markdown

--- js/app.js
+++ js/app.js
@@ -2,9 +2,53 @@
 	"use strict";
 	var app = {
 		init:function(){
-			// let's go
+			app.listeners();
+
 		},
-	};
+		listeners : function(){
+			$('#putArticle').on('click', app.functionAjax);
+			$('#converter').on('click', app.printArticle(app.printArticle.res));
+		},
+		functionAjax : function(){
+			$.ajax('http://192.168.1.21:1337/alice.md')
+			.done(function(response){
+				$('#montreMoi').html(response)
+			})
+			.fail(app.failed())
+			.always(app.always());
+			
+		},
+		printArticle : function(){
+			console.log();
+		},
+		success : function(response){
+			console.log(response);
+			console.log('réussi');
+		},
+		failed : function(){
+			console.log('échec');
+		},
+		always : function(){
+			
+		},
+		converter : function(alice){
+			var converter = new showdown.Converter(),
+			text = alice,
+			html = converter.makeHtml(text);
+			$('#md').html(html);
+		}	
+
+		
+	}
+
+
+
+
+
+
+
+
+
 
 
 	$(document).ready(function(){
