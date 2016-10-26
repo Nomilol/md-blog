94d657261be2902660bf6c20c11bd5e319055c93
Axel <lezinaxel@gmail.com>
2016-10-26 13:44:53 +0200

    Epuration du code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log('start');
 (function(){
 	"use strict";
 	var app = {
@@ -8,14 +7,10 @@ console.log('start');
 		},
 		listeners : function(){
 			$('#putArticle').on('click', app.functionAjax);
-			
 		},
 		functionAjax : function(){
 			$.ajax('http://192.168.1.40:1337/alice.md')
 			.done(app.success)
-			
-			
-			
 		},
 		success : function(response){
 			var converter = new showdown.Converter(),
@@ -23,19 +18,11 @@ console.log('start');
 			html = converter.makeHtml(text);
 			$('#md').html(html);
 		},
-		failed : function(){
-			console.log('échec');
-		},
-		always : function(){
-			
-		},
 		functionAppelJSON : function(){
 			$.ajax('http://192.168.1.40:1337/menu.json')
 			.done(app.successJSON)
-			
 		},
 		successJSON : function(response){
-			console.log('successJSON');
 			for(var i = 0 ; i < response.menu.length ; i++){
 				var responseJSON = response.menu[i];
 				$('ul').append('<li>' + '<button>' + responseJSON.title + '</button>' + '</li>');
@@ -45,26 +32,13 @@ console.log('start');
 				$('.lienArticle0').on('click',function(){
 					$.ajax('http://192.168.1.40:1337/example.md')
 					.done(app.success);
-					
 				});
-
 				$('.lienArticle1').on('click', function(){
 					$.ajax('http://192.168.1.40:1337/alice.md')
 					.done(app.success)
-					
 				});
-
-
 			}
 		},
-		failedJSON : function(){
-			console.log('échecJSON');
-		},
-		functionRedirection : function(){
-
-			console.log('functionRedirection');
-		}
-
 	};
 	$(document).ready(function(){
 		app.init();
